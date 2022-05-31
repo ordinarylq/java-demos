f15922230fafbfb52404ad3ce5294462b744dbbb
liqi <973129554@qq.com>
2022-05-31 16:58:28 +0800

    修改spring security

--- spring-security-demo01/src/main/java/com/lq/security/config/WebAppSecurityConfig.java
+++ spring-security-demo01/src/main/java/com/lq/security/config/WebAppSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.lq.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @ClassName: WebAppSecurityConfig
@@ -12,5 +14,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @Configuration
 @EnableWebSecurity
-public class WebAppSecurityConfig {
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/layui/**", "/index.jsp")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index.jsp")
+                .permitAll();
+    }
 }
