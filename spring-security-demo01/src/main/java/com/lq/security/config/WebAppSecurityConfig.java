861bdd88c49f25518131db2acd8e1deeddbc4a4d
liqi <973129554@qq.com>
2022-05-31 16:38:47 +0800

    Spring Security demo初次提交

---
+++ spring-security-demo01/src/main/java/com/lq/security/config/WebAppSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.lq.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * @ClassName: WebAppSecurityConfig
+ * @Author: LiQi
+ * @Date: 2022-05-31 16:34
+ * @Version: V1.0
+ * @Description:
+ */
+@Configuration
+@EnableWebSecurity
+public class WebAppSecurityConfig {
+}
