fadcfc3831d4b3de858c04d9b4c384fe227e375e
Li Qi <973129554@qq.com>
2022-08-28 22:21:27 +0800

    add Spring Cloud Config demo

---
+++ cloud-demo/cloud-config-center-3344/src/main/java/com/lq/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.lq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author qili
+ * @create 2022-08-28-18:16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-demo/cloud-config-client-3355/src/main/java/com/lq/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.lq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author qili
+ * @create 2022-08-28-22:06
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
